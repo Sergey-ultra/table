650e09031b303e8dddf46d35d984107c727cb225
Lenovo <maasa@list.ru>
2020-03-19 22:45:21 +0400

    Updates

---
+++ precache-manifest.b73c2e668ab0dfc6e1c0c48d93e8216e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809e85208564034a56019bdf711b4b80",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "97becb9dba45529cb7de",
+    "url": "/table/static/css/main.478622fc.chunk.css"
+  },
+  {
+    "revision": "1c2b3d03593b43b576ee",
+    "url": "/table/static/js/2.49070a06.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/table/static/js/2.49070a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97becb9dba45529cb7de",
+    "url": "/table/static/js/main.57d98b0f.chunk.js"
+  },
+  {
+    "revision": "f607595d2418436059aa",
+    "url": "/table/static/js/runtime-main.23bda436.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/table/static/media/1_9EBHIOzhE1XfMYoKz1JcsQ.26ef8bb4.gif"
+  }
+]);
